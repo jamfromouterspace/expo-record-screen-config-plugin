8c21f661a68d29b69502fd867fc25ec912339d06
Jamiel Rahi <jamiel.nr@gmail.com>
2023-08-31 19:29:03 -0700

    add ReplayKit to xcode frameworks"

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,17 +1,24 @@
-import { ConfigPlugin, withXcodeProject } from '@expo/config-plugins';
+import { ConfigPlugin, withXcodeProject, IOSConfig } from '@expo/config-plugins';
 
-const withMyApiKey: ConfigPlugin = config => {
+const withReplayKit: ConfigPlugin = config => {
   return withXcodeProject(
     config,
     async (
       config
     ) => {
-      const xcodeProject = config.modResults;
-      console.log("xcodeProject", JSON.stringify(xcodeProject))
-
+      if (!config.modRequest.projectName) {
+        // error
+        throw new Error("Cannot find project name")
+      }
+      let xcodeProject = config.modResults;
+      const target = IOSConfig.XcodeUtils.getApplicationNativeTarget({
+          project: config.modResults,
+          projectName: config.modRequest.projectName,
+      });
+      xcodeProject.addFramework("ReplayKit.framework", { target: target.uuid, weak: true });
       return config;
     }
   );
 };
 
-export default withMyApiKey;
+export default withReplayKit;
